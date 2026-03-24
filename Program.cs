1c0cd8ef56cfc16279041480ab7bea7847778d28
karlafraniczavadlav1104 <karla.franic.zavadlav@gmail.com>
2026-03-24 17:19:57 +0100

    test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pis1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
