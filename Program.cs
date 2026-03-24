98ba7679e02d5c7a5843bfee7e2399f77de40a55
KristijanPerkovic <kperko01@fesb.hr>
2026-03-24 17:52:53 +0100

    Dodao sam ide ide i komentar nije lose

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace pis1
             Console.WriteLine("kako si");
             Console.WriteLine("kako si");
             Console.WriteLine("kako si");
+            Console.WriteLine("ide ide");
+            //nije lose
         }
     }
 }
